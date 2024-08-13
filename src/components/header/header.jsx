0bbb2d658d4670c9a690f33207924020fbc7283e
Connie <connieramirezphotography@gmail.com>
2024-08-13 13:48:28 +0100

    First try to header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import files from  ".styless/module.css";
+
+export default function Header ({notText}) {
+    return (        
+        <header className="header-container">
+              <h1>🔥 Fireplace Palace</h1>
+            </header>
+    );
+}
+
