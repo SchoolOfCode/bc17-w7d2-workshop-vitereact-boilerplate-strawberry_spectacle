08ee89386adef6591e1fd13bf989f8ba2289969f
Connie <connieramirezphotography@gmail.com>
2024-08-14 22:58:42 +0100

    updated footer and content added to Founders section

--- my-app/app/founders/page.js
+++ my-app/app/founders/page.js
@@ -1,6 +1,29 @@
+import Image from "next/image";
 
 export default function Founders() {
   return (
-   <h1>This is our history</h1>
+      <><section className="IntroFounders">
+      <h2>Meet the artisans behind our masterpieces!</h2>
+      <p>Mike and Mandy</p>
+      <img srcSet="founder-mike-and-mandy.png" alt="Mike and Mandy"/>
+      </section>
+      <section className="ContentFounders">
+          <div>
+          <h3>Our craftsmanship</h3>
+          <p>Mike and Mandy studied and honed their craft under the fireplace sensei Vik Von Blaze. Nothing gets delivered to a customer without their sign off.</p>
+          <img src="founders-1.png" alt="Craftsmanship Image"/>
+          </div>
+        <div>
+          <h3>Our experience</h3>
+          <p>Numbers don't lie - we've been around for 20+ years and have a long list of happy customers who gladly recommend us.</p>
+          <img src="founders-2.png" alt="Experience Image"/>
+          </div>
+        <div>
+          <h3>Our guarantee</h3>
+          <p>If you're not 100% satisfied we will fully refund your purchase. Also, we offer free repairs for the first 20 years of ownership. Find that somewhere else!</p>
+          <img src="founders-3.png" alt="Guarantee Image"/>
+          </div>
+      </section>
+      </>
   );
 }
--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -1,33 +1,39 @@
 
 
-export default function Footer () {
-    return (        
-        <footer className="footer">
-        <div className="colunms-container">
-          <div className="footer-column">
-            <p>Find us on:</p>
-            <ul className="footer-nav">
-              <li>
-                <a href="https://www.facebook.com" className="socials-link">Facebook</a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com" className="socials-link"
-                  >Instagram</a>
-              </li>
-              <li>
-                <a href="https://www.tiktok.com" className="socials-link">TikTok</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="footer-column">
-            <p>Contact us:</p>
-            <address>
-              <a href="mailto:info@firepalace.co.uk" className="email-link"
-                >info@firepalace.co.uk</a>
-            </address>
-          </div>
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="columnsContainer">
+        <div className="footerColumn">
+          <p>Find us on:</p>
+          <ul className="footerNav">
+            <li>
+              <a href="https://www.facebook.com" className="socialsLink">
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com" className="socialsLink">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://www.tiktok.com" className="socialsLink">
+                TikTok
+              </a>
+            </li>
+          </ul>
         </div>
-      </footer>
-    );
+
+        <div className="footerColumn">
+          <p>Contact us:</p>
+          <address>
+            <a href="mailto:info@firepalace.co.uk" className="emailLink">
+              info@firepalace.co.uk
+            </a>
+          </address>
+        </div>
+      </div>
+    </footer>
+  );
 }
