76a113e1ab55e707cb97854b6df424edc26b4020
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-08-15 13:30:00 +0100

    working menu toggle and links

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,25 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "@/src/components/Hero/Hero";
+import Link from "next/link";
+
+function handleClick() {
+  console.log('TESTING 123');
+};
+
 
 export default function Home() {
   return (
+
+   <main>
+   <Link href="/founders" onClick={handleClick} >Founders Button</Link>
    <Hero />
+   </main>
   );
 }
+
+//set up a LINK href in page.js
+  //add a Next.js "<Link href="/founders"></Link>" component 
+//Create a LINK file
+//IMPORT LINK FILE
