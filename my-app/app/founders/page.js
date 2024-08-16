c06214db1c44d5be5ebf06d0c564237d65a5e4aa
Connie <connieramirezphotography@gmail.com>
2024-08-16 23:31:30 +0100

    looking pretty, css updated

--- my-app/app/founders/page.js
+++ my-app/app/founders/page.js
@@ -1,27 +1,28 @@
 import Image from "next/image";
+import styles from '../page.module.css';
 
 export default function Founders() {
   return (
-      <><section className="IntroFounders">
+      <><section className={styles.introFounders}>
       <h2>Meet the artisans behind our masterpieces!</h2>
       <p>Mike and Mandy</p>
-      <img srcSet="founder-mike-and-mandy.png" alt="Mike and Mandy"/>
+      <Image src='/founder-mike-and-mandy.png' width={500} height={500} alt="Mike and Mandy"/>
       </section>
-      <section className="ContentFounders">
+      <section className={styles.contentFounders}>
           <div>
-          <h3>Our craftsmanship</h3>
+          <h3 className={styles.contentFoundersH3}>Our craftsmanship</h3>
           <p>Mike and Mandy studied and honed their craft under the fireplace sensei Vik Von Blaze. Nothing gets delivered to a customer without their sign off.</p>
-          <img src="founders-1.png" alt="Craftsmanship Image"/>
+          <Image src="/founders-1.png" width={400} height={224} alt="Craftsmanship Image"/>
           </div>
         <div>
-          <h3>Our experience</h3>
+          <h3 className={styles.contentFoundersH3}>Our experience</h3>
           <p>Numbers don't lie - we've been around for 20+ years and have a long list of happy customers who gladly recommend us.</p>
-          <img src="founders-2.png" alt="Experience Image"/>
+          <Image src="/founders-2.png" width={400} height={224} alt="Experience Image"/>
           </div>
         <div>
-          <h3>Our guarantee</h3>
+          <h3 className={styles.contentFoundersH3}>Our guarantee</h3>
           <p>If you're not 100% satisfied we will fully refund your purchase. Also, we offer free repairs for the first 20 years of ownership. Find that somewhere else!</p>
-          <img src="founders-3.png" alt="Guarantee Image"/>
+          <Image src="/founders-3.png" width={400} height={224} alt="Guarantee Image"/>
           </div>
       </section>
       </>
